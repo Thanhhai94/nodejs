aabdde2c2377d4852bfe5ec1e3c2aea54587129a
thanhhai <thanhhai.utc.k53@gmail.com>
2022-05-18 21:54:37 +0700

    lab3.6

--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,7 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.render('add-product',{pageTitle: 'add product'});
+  res.render('add-product',{pageTitle: 'add product', path: '/admin/add-product'});
 });
 
 // /admin/add-product => POST
